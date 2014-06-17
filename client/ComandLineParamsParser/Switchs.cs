61a96e35fd99a470e6f92e297b72e252f53e6aa0
Roman Klassen <klassen.roman@comp-gluk.ru>
2014-06-17 22:36:50 +0400

    Парсинг параметров командной строки для эмулятора клиентов

---
+++ client/ComandLineParamsParser/Switchs.cs
@@ -0,0 +1,61 @@
+#region Copyright
+/*
+ * Copyright 2013-2018 Roman Klassen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+#endregion
+
+﻿namespace client.ComandLineParamsParser
+{
+    /// <summary>
+    ///     Параметры запуска
+    /// </summary>
+    internal enum ComandSwitch
+    {
+        None,
+        ClientCount,
+        QueryPerClient,
+        BalancerHost,
+        BalancerPort
+    }
+
+    /// <summary>
+    ///     Ключи параметров
+    /// </summary>
+    internal static class Switchs
+    {
+        public static ComandSwitch Parse(string input)
+        {
+            switch (input)
+            {
+                case "-c":
+                case "--clients":
+                    return ComandSwitch.ClientCount;
+                case "-q":
+                case "--queries":
+                    return ComandSwitch.QueryPerClient;
+                case "-h":
+                case "--host":
+                    return ComandSwitch.BalancerHost;
+                case "-p":
+                case "--port":
+                    return ComandSwitch.BalancerPort;
+                default:
+                    return ComandSwitch.None;
+            }
+            return ComandSwitch.None;
+        }
+    }
+}
