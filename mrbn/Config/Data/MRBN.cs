d093f6a66f6d27b3c3b4d9c558a2b04752dc47af
Roman Klassen <klassen.roman@comp-gluk.ru>
2014-06-22 21:08:04 +0400

    Начата работа над МРБН

---
+++ mrbn/Config/Data/MRBN.cs
@@ -0,0 +1,9 @@
+﻿namespace mrbn.Config.Data
+{
+    public class MRBN
+    {
+        public uint GlobalId { get; set; }
+        public uint RegionId { get; set; }
+        public uint Port { get; set; }
+    }
+}
---
+++ mrbn/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Text;
+using Balancer.Common;
+using mrbn.Config;
+using mrbn.Config.Data;
+using mrbn.Properties;
+
+namespace mrbn
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.SetLogFile("mrbnLog.txt");
+            Logger.Write("Сервер запущен");
+
+            const string configFilePath = "mrbnConfig.xml";
+            if (!File.Exists(configFilePath))
+            {
+                MRBNConfig.Load(
+                    new MemoryStream(Encoding.UTF8.GetBytes(Resources.defaultConfig))).Save(configFilePath);
+            }
+            MRBNConfig.Load(configFilePath);
+        }
+    }
+}
