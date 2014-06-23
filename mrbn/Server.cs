0b12fc8e500d6285db245ef28094a59d6ba18b4b
Roman Klassen <klassen.roman@comp-gluk.ru>
2014-06-23 21:41:09 +0400

    Сервер МРБН

---
+++ mrbn/Server.cs
@@ -0,0 +1,63 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Balancer.Common;
+
+namespace mrbn
+{
+    /// <summary>
+    ///     РБН
+    /// </summary>
+    internal class Server
+    {
+        /// <summary>
+        ///     Слушатель новых соединений (клиентов)
+        /// </summary>
+        private readonly TcpListener _listener;
+
+        /// <summary>
+        ///     признак жизн потока
+        /// </summary>
+        private bool _serverIsLife;
+
+
+        public Server(int port)
+        {
+            _listener = new TcpListener(IPAddress.Any, port);
+            _listener.Start();
+            Logger.Write("Начато прослушивание " + IPAddress.Any + ":" + port);
+
+            _serverIsLife = true;
+
+
+            while (_serverIsLife)
+            {
+                TcpClient tcpClient = _listener.AcceptTcpClient();
+                Logger.Write("Принято соединие");
+
+                var t = new Thread(ClientThread);
+                t.Start(tcpClient);
+            }
+        }
+
+        /// <summary>
+        ///     Поток обслуживания клиентского соедиения
+        /// </summary>
+        /// <param name="param"></param>
+        private void ClientThread(object param)
+        {
+        }
+
+        /// <summary>
+        ///     убийца - деструктор
+        /// </summary>
+        ~Server()
+        {
+            _serverIsLife = false;
+            if (_listener != null)
+            {
+                _listener.Stop();
+            }
+        }
+    }
+}
