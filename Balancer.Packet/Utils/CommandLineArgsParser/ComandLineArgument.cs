53b8093295ac69222cd7148e35166567d7faa8c9
Roman Klasen <klassen.roman@comp-gluk.ru>
2014-12-14 15:37:29 +0300

    Добавлен парсер для командной строки

---
+++ Balancer.Packet/Utils/CommandLineArgsParser/ComandLineArgument.cs
@@ -0,0 +1,34 @@
+﻿namespace Balancer.Common.Utils.CommandLineArgsParser
+{
+    public class ComandLineArgument
+    {
+        public ComandLineArgument(string argument)
+        {
+            Argument = argument;
+            Value = string.Empty;
+        }
+
+        public ComandLineArgument(string argument, string[] alternativeArguments) :
+            this(argument)
+        {
+            AlternativeArguments = alternativeArguments;
+        }
+
+        public string Argument { get; protected set; }
+        public string[] AlternativeArguments { get; protected set; }
+        public string Value { get; protected set; }
+        public bool IsDefined { get; protected set; }
+
+        public void Define(string value)
+        {
+            IsDefined = true;
+            Value = value;
+        }
+
+        public void UnDefine()
+        {
+            IsDefined = false;
+            Value = string.Empty;
+        }
+    }
+}
---
+++ Balancer.Packet/Utils/CommandLineArgsParser/CommandLineArgumentsParser.cs
@@ -0,0 +1,54 @@
+﻿namespace Balancer.Common.Utils.CommandLineArgsParser
+{
+    public class CommandLineArgumentsParser
+    {
+        private ComandLineArgument[] ComandLineArguments;
+
+        public CommandLineArgumentsParser(ComandLineArgument[] comandLineArguments)
+        {
+            ComandLineArguments = comandLineArguments;
+        }
+
+        public ComandLineArgument[] Parse(string[] args)
+        {
+            ClearParse();
+            for (int i = 0; i < args.Length; i++)
+            {
+                CheckArgument(args[i], i + 1 < args.Length ? args[i + 1] : string.Empty);
+            }
+            return ComandLineArguments;
+        }
+
+        private void ClearParse()
+        {
+            foreach (ComandLineArgument comandLineArgument in ComandLineArguments)
+            {
+                comandLineArgument.UnDefine(); 
+            }
+        }
+
+        private void CheckArgument(string argument, string nextArgument)
+        {
+            string arg = argument.ToLower();
+            foreach (ComandLineArgument comandLineArgument in ComandLineArguments)
+            {
+                if (arg == comandLineArgument.Argument.ToLower())
+                {
+                    comandLineArgument.Define(nextArgument);
+                    return;
+                }
+                else
+                {
+                    foreach (var alternativeAgument in comandLineArgument.AlternativeArguments)
+                    {
+                        if (arg == alternativeAgument.ToLower())
+                        {
+                            comandLineArgument.Define(nextArgument);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
