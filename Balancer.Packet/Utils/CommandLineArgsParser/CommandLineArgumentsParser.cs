0b5ab738cc94c2a37e2d9c871a4abf82b698eac3
Roman Klassen <klassen.roman@comp-gluk.ru>
2015-01-03 12:18:58 +0300

    Добавлен вывод справки по аргументам командной строки

--- Balancer.Packet/Utils/CommandLineArgsParser/CommandLineArgumentsParser.cs
+++ Balancer.Packet/Utils/CommandLineArgsParser/CommandLineArgumentsParser.cs
@@ -1,4 +1,6 @@
-﻿namespace Balancer.Common.Utils.CommandLineArgsParser
+﻿using System.Collections.Generic;
+
+namespace Balancer.Common.Utils.CommandLineArgsParser
 {
     public class CommandLineArgumentsParser
     {
@@ -7,6 +9,41 @@
         public CommandLineArgumentsParser(ComandLineArgument[] comandLineArguments)
         {
             ComandLineArguments = comandLineArguments;
+        }
+
+        public string[] GetDescriptions()
+        {
+            var descriptions = new List<string>();
+            int maxArgumentLength = FindMaxArgumentLenght(ComandLineArguments);
+
+            foreach (var comandLineArgument in ComandLineArguments)
+            {
+                string spaces = string.Empty;
+                string argument = comandLineArgument.GetArgumentString();
+                int spaceLenght = maxArgumentLength - argument.Length;
+
+                for (int i = 0; i < spaceLenght; i++)
+                {
+                    spaces += " ";
+                }
+
+                string description = string.Format("{0}{2} - {1}", argument, comandLineArgument.Description, spaces);
+                descriptions.Add(description);
+            }
+
+            return descriptions.ToArray();
+        }
+
+        private int FindMaxArgumentLenght(ComandLineArgument[] comandLineArguments)
+        {
+            int maxLenght = 0;
+            foreach (var comandLineArgument in comandLineArguments)
+            {
+                int lenght = comandLineArgument.GetArgumentString().Length;
+
+                if (maxLenght < lenght) maxLenght = lenght;
+            }
+            return maxLenght;
         }
 
         public ComandLineArgument[] Parse(string[] args)
@@ -37,15 +74,12 @@
                     comandLineArgument.Define(nextArgument);
                     return;
                 }
-                else
+                foreach (var alternativeAgument in comandLineArgument.AlternativeArguments)
                 {
-                    foreach (var alternativeAgument in comandLineArgument.AlternativeArguments)
+                    if (arg == alternativeAgument.ToLower())
                     {
-                        if (arg == alternativeAgument.ToLower())
-                        {
-                            comandLineArgument.Define(nextArgument);
-                            return;
-                        }
+                        comandLineArgument.Define(nextArgument);
+                        return;
                     }
                 }
             }
